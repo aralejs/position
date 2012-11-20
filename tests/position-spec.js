34101c8d2d6e8f1ff831884fb0706a64015f59de
pianyou <afcstar@163.com>
2012-11-20 16:53:45 +0800

    用例改用mocha 添加了几个用例

--- tests/position-spec.js
+++ tests/position-spec.js
@@ -2,18 +2,20 @@ define(function(require) {
 
     var Position = require('../src/position');
     var $ = require('$');
-    var expect = chai.expect;
 
     describe('position', function() {
 
         var pinElement, baseElement, noopDiv;
-        $(document.body).css('margin', 0);
+        $(document.body).css({
+            'margin': 0,
+            'padding': 0
+        });
 
         beforeEach(function() {
-            pinElement = $('<div style="width:100px;height:100px;">pinElement</div>').prependTo(document.body);
+            pinElement = $('<div style="width:100px;height:100px;" id="pin-element">pinElement</div>').prependTo(document.body);
             // for ie6 bug
             noopDiv = $('<div></div>').prependTo(document.body);
-            baseElement = $('<div style="margin:20px;border:5px solid #000;padding:20px;width:200px;height:200px;">baseElement</div>').prependTo(document.body);            
+            baseElement = $('<div style="margin:20px;border:5px solid #000;padding:20px;width:200px;height:200px;" id="base-element">baseElement</div>').prependTo(document.body);            
         });
 
         afterEach(function() {
@@ -40,8 +42,32 @@ define(function(require) {
             expect(pinElement.offset().left).to.equal(120);
         });
 
+        it('第二个参数简略写法：Position.pin({ element:pinElement, x: -100, y: -100 }, baseElement)', function() {
+            Position.pin({ element:pinElement, x: -100, y: -100 }, baseElement);
+            expect(pinElement.offset().top).to.equal(120);
+            expect(pinElement.offset().left).to.equal(120);
+        });
+
+        it('两个参数都简略：Position.pin(pinElement, baseElement)', function() {
+            Position.pin(pinElement, baseElement);
+            expect(pinElement.offset().top).to.equal(20);
+            expect(pinElement.offset().left).to.equal(20);
+        });
+
         it('带px的字符串参数：Position.pin(pinElement, { element:baseElement, x: "100px", y: "100px" })', function() {
             Position.pin({ element: pinElement, x: 0, y: 0 }, { element:baseElement, x: "100px", y: "100px" });
+            expect(pinElement.offset().top).to.equal(120);
+            expect(pinElement.offset().left).to.equal(120);
+        });
+
+        it('参数传递选择器：Position.pin("#pin-element", { element:"#base-element", x: "100px", y: "100px" })', function() {
+            Position.pin('#pin-element', { element:'#base-element', x: "100px", y: "100px" });
+            expect(pinElement.offset().top).to.equal(120);
+            expect(pinElement.offset().left).to.equal(120);
+        });
+
+        it('参数传递原生对象：Position.pin(pinElement[0], { element:baseElement[0], x: "100px", y: "100px" })', function() {
+            Position.pin(pinElement[0], { element:baseElement[0], x: "100px", y: "100px" });
             expect(pinElement.offset().top).to.equal(120);
             expect(pinElement.offset().left).to.equal(120);
         });
@@ -64,18 +90,30 @@ define(function(require) {
             expect(pinElement.offset().left).to.equal(-230);
         });
 
+        it('很大的百分比：Position.pin(pinElement, { element:baseElement, x: "1000%", y: "500%" })', function() {
+            Position.pin(pinElement, { element:baseElement, x: '1000%', y: '500%' });
+            expect(pinElement.offset().top).to.equal(1270);
+            expect(pinElement.offset().left).to.equal(2520);
+        });
+
         it('别名：Position.pin({ element:pinElement, x: "left", y: "left" }, { element:baseElement, x: "right", y: "center" })', function() {
             Position.pin({ element:pinElement, x: "left", y: "left" }, { element:baseElement, x: 'right', y: 'center' });
             expect(pinElement.offset().top).to.equal(145);
             expect(pinElement.offset().left).to.equal(270);
         });
 
+        it('大写的别名：Position.pin({ element:pinElement, x: "LEFT", y: "LEFT" }, { element:baseElement, x: "RIGHT", y: "CENTER" })', function() {
+            Position.pin({ element:pinElement, x: "LEFT", y: "LEFT" }, { element:baseElement, x: 'RIGHT', y: 'CENTER' });
+            expect(pinElement.offset().top).to.equal(145);
+            expect(pinElement.offset().left).to.equal(270);
+        });
+
         it('百分比小数：Position.pin(pinElement, { element:baseElement, x: "99.5%", y: "50.5%" })', function() {
             Position.pin(pinElement, { element:baseElement, x: "99.5%", y: "50.5%" });
-            expect(pinElement.offset().top).to.equalGreaterThan(145.99);
-            expect(pinElement.offset().top).to.equalLessThan(147.01);            
-            expect(pinElement.offset().left).to.equalGreaterThan(267.99);
-            expect(pinElement.offset().left).to.equalLessThan(269.01);
+            expect(pinElement.offset().top).to.be.greaterThan(145.99);
+            expect(pinElement.offset().top).to.be.lessThan(147.01);            
+            expect(pinElement.offset().left).to.be.greaterThan(267.99);
+            expect(pinElement.offset().left).to.be.lessThan(269.01);
         });
 
         it('居中定位：Position.center(pinElement, baseElement);', function() {
@@ -84,12 +122,34 @@ define(function(require) {
             expect(pinElement.offset().left).to.equal(95);
         });
 
-        it('屏幕居中定位：Position.center(pinElement );', function() {
+        it('屏幕居中定位：Position.center(pinElement);', function() {
             Position.center(pinElement);
-            expect(($(window).outerHeight()-100)/2).to.equalGreaterThan(pinElement.offset().top-0.51);
-            expect(($(window).outerHeight()-100)/2).to.equalLessThan(pinElement.offset().top+0.51);
-            expect(($(window).outerWidth()-100)/2).to.equalGreaterThan(pinElement.offset().left-0.51);
-            expect(($(window).outerWidth()-100)/2).to.equalLessThan(pinElement.offset().left+0.51); 
+            expect(($(window).outerHeight()-100)/2).to.be.greaterThan(pinElement.offset().top-0.51);
+            expect(($(window).outerHeight()-100)/2).to.be.lessThan(pinElement.offset().top+0.51);
+            expect(($(window).outerWidth()-100)/2).to.be.greaterThan(pinElement.offset().left-0.51);
+            expect(($(window).outerWidth()-100)/2).to.be.lessThan(pinElement.offset().left+0.51); 
+        });
+
+        it('baseElement绝对定位：', function() {
+            baseElement.css({
+                'position': 'absolute',
+                'top': '20px',
+                'left': '30px'
+            });
+            Position.pin(pinElement, { element:baseElement, x: 100, y: 100 });
+            expect(parseInt(pinElement.offset().top)).to.equal(140);
+            expect(parseInt(pinElement.offset().left)).to.equal(150);
+        });
+
+        it('baseElement相对定位：', function() {
+            baseElement.css({
+                'position': 'relative',
+                'top': '-20px',
+                'left': '30px'
+            });
+            Position.pin(pinElement, { element:baseElement, x: 100, y: 100 });
+            expect(parseInt(pinElement.offset().top)).to.equal(100);
+            expect(parseInt(pinElement.offset().left)).to.equal(150);
         });
 
         it('offsetParent不为body：', function() {
@@ -103,6 +163,15 @@ define(function(require) {
 
         it('offsetParent绝对定位：', function() {
             var offsetParent = $('<div style="position:absolute;top:50px;left:50px;"></div>').appendTo(document.body);
+            baseElement.appendTo(offsetParent);
+            Position.pin(pinElement, { element:baseElement, x: 100, y: 100 });
+            expect(parseInt(pinElement.offset().top)).to.equal(170);
+            expect(parseInt(pinElement.offset().left)).to.equal(170);
+            offsetParent.remove();
+        });
+
+        it('offsetParent相对定位：', function() {
+            var offsetParent = $('<div style="position:relative;top:50px;left:50px;"></div>').prependTo(document.body);
             baseElement.appendTo(offsetParent);
             Position.pin(pinElement, { element:baseElement, x: 100, y: 100 });
             expect(parseInt(pinElement.offset().top)).to.equal(170);
@@ -128,11 +197,10 @@ define(function(require) {
             expect(parseInt(pinElement.offset().left)).to.equal(260);
         });
 
-        it('相对自身定位：', function() {
-            baseElement.remove();
-            Position.pin(pinElement, { element:pinElement, x: "100%", y: 0 });
-            expect(parseInt(pinElement.offset().top)).to.equal(0);
-            expect(parseInt(pinElement.offset().left)).to.equal(100);
+        it('0% 的情况', function() {
+            Position.pin(pinElement, { element:baseElement, x: "0%", y: '0%' });
+            expect(parseInt(pinElement.offset().top)).to.equal(20);
+            expect(parseInt(pinElement.offset().left)).to.equal(20);
         });
 
         it('fixed定位：', function() {
@@ -155,3 +223,4 @@ define(function(require) {
 
     });
 });
+
